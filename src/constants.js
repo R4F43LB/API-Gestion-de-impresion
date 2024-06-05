447863778989893530f1396e7c29ba8082c30814
Rafael Balestrini <152675629+R4F43LB@users.noreply.github.com>
2024-06-05 16:34:16 -0400

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// by Rafael Balestrini (2021)
+// The global constants must be declared here
+
+module.exports = {
+  PRINT_REQUEST_RECEIVED: 10,
+  SUBSTRATE_PRINTING: 20,
+  PRINTED_SUBSTRATE: 30,
+  SUBSTRATE_SHIPPED: 40,
+  SUBSTRATE_RETURNED: 50,
+
+  USER: 0,
+  ADMIN: 1,
+
+  REGEXPDATE: '(19|20)\\d{2}[-](0[1-9]|1[0-2])[-](0[1-9]|1[0-9]|2[0-9]|3[01])'
+};
