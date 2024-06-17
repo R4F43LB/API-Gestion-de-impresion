62a7cfc2640304ac6d8b18393328a3937c90891a
Rafael Balestrini <152675629+R4F43LB@users.noreply.github.com>
2024-06-17 10:49:56 -0400

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,26 @@
+// by Rafael Balestrini
+// This code will connect our API to the 'glc' mongo database at startup.
+
+const mongoose = require('mongoose');
+
+module.exports = {
+  connect: DB_HOST => {
+    // Use the Mongo driver's updated URL string parser
+    mongoose.set('useNewUrlParser', true);
+    // Use findOneAndUpdate() in place of findAndModify()
+    mongoose.set('useFindAndModify', false);
+    // Use createIndex() in place of ensureIndex()
+    mongoose.set('useCreateIndex', true);
+    // Use the new server discovery and monitoring engine
+    mongoose.set('useUnifiedTopology', true);
+    // Connect to the DB
+    mongoose.connect(DB_HOST);
+    // Log an error if we fail to connect
+    mongoose.connection.on('error', err => {
+      console.error(err);
+      console.log('MongoDB connection error. Please make sure MongoDB is running.');
+      process.exit();
+    });
+  },
+  close: () => {mongoose.connection.close();}
+};
