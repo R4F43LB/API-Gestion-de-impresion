e0cbdcce75f7c4f7d227bce5aaf80c9b83d85f49
Rafael Balestrini <152675629+R4F43LB@users.noreply.github.com>
2024-07-05 23:01:09 -0400

    Create index.js

---
+++ src/resolvers/index.js
@@ -0,0 +1,16 @@
+// by Rafael Balestrini 25/11/202
+// This file combine our resolvers into a single JavaScript module. While this isn’t
+// strictly necessary, it's a good pattern to follow as this application and its
+// resolvers schemas grow.
+
+const Query = require('./query');
+const Mutation = require('./mutation');
+const License = require('./license');
+const {GraphQLDateTime} = require('graphql-iso-date');
+
+module.exports = {
+  Query,
+  Mutation,
+  License,
+  DateTime: GraphQLDateTime
+};
