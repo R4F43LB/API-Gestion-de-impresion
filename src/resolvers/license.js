9bf48f3cc39b4464be99cc9cac86a9c96b87fd91
Rafael Balestrini <152675629+R4F43LB@users.noreply.github.com>
2024-07-07 01:44:50 -0400

    Create license.js

---
+++ src/resolvers/license.js
@@ -0,0 +1,26 @@
+// by Rafael Balestrini 25/11/20
+// Code needed to resolve the users and status info for a license when requested
+
+const appCons = require('../constants'); //Our app constants
+module.exports = {
+  // Resolve the owner info for a license when requested
+  CUSTOMER: async (license, args, {models}) => {
+    return await models.User.findById(license.CUSTOMER);
+  },
+  // Resolve the status info for a license when requested
+  STATUS_LICENSE: async (license, args, {models, user}) => {
+    let u = await models.User.findById(user.id); // find the user
+    if (u.access !== appCons.ADMIN)
+      return await models.Status.find(
+        {
+          license: license._id,
+          $or: [
+            {code: appCons.PRINT_REQUEST_RECEIVED},
+            {code: appCons.SUBSTRATE_SHIPPED},
+            {code: appCons.SUBSTRATE_RETURNED}
+          ]
+        }
+      )
+    return await models.Status.find({license: license._id})
+  }
+};
